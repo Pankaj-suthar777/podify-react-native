3b8a7fa9142289f12475ec50e7ae4ad50866f7bd
pankaj suthar <pankajss0070@gmail.com>
2024-07-07 15:59:11 +0530

    favorite audio

--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -1,5 +1,6 @@
 import { isValidObjectId } from "mongoose";
 import * as yup from "yup";
+import { categories } from "./audio_category";
 
 export const CreateUserSchema = yup.object().shape({
   name: yup
@@ -52,4 +53,13 @@ export const UpdatePassowrdSchema = yup.object().shape({
 export const SignInValidationSchema = yup.object().shape({
   email: yup.string().required("Email is missing!").email("Invalid email id!"),
   password: yup.string().trim().required("Password is missing!"),
+});
+
+export const AudioValidationSchema = yup.object().shape({
+  title: yup.string().required("Title is missing!"),
+  about: yup.string().required("About is missing!"),
+  category: yup
+    .string()
+    .oneOf(categories, "Invalid category!")
+    .required("Category is missing!"),
 });
