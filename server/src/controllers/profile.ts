a88e23175b1082195b21f96c81ca45e3ccced2c1
pankaj suthar <pankajss0070@gmail.com>
2024-07-07 18:16:34 +0530

    get public profile

--- server/src/controllers/profile.ts
+++ server/src/controllers/profile.ts
@@ -1,6 +1,8 @@
+import { paginationQuery } from "#/@types/misc";
+import Audio, { AudioDocument } from "#/models/audio";
 import User from "#/models/user";
 import { RequestHandler } from "express";
-import { isValidObjectId } from "mongoose";
+import { isValidObjectId, ObjectId } from "mongoose";
 
 export const updateFollower: RequestHandler = async (req, res) => {
   const { profileId } = req.params;
@@ -65,4 +67,80 @@ export const updateFollower: RequestHandler = async (req, res) => {
   }
 
   res.json({ status });
+};
+
+export const getUploads: RequestHandler = async (req, res) => {
+  const { limit = "20", pageNo = "0" } = req.query as paginationQuery;
+
+  const data = await Audio.find({ owner: req.user.id })
+    .skip(parseInt(limit) * parseInt(pageNo))
+    .limit(parseInt(limit))
+    .sort("-createdAt");
+
+  const audios = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      about: item.about,
+      file: item.file.url,
+      poster: item.poster?.url,
+      date: item.createdAt,
+      owner: { name: req.user.name, id: req.user.id },
+    };
+  });
+
+  res.json({ audios });
+};
+
+export const getPublicUploads: RequestHandler = async (req, res) => {
+  const { limit = "20", pageNo = "0" } = req.query as paginationQuery;
+
+  const { profileId } = req.params;
+
+  if (!isValidObjectId(profileId)) {
+    return res.json(422).json({ error: "Invalid profile id!" });
+  }
+
+  const data = await Audio.find({ owner: profileId })
+    .skip(parseInt(limit) * parseInt(pageNo))
+    .limit(parseInt(limit))
+    .sort("-createdAt")
+    .populate<AudioDocument<{ name: string; _id: ObjectId }>>("owner");
+
+  const audios = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      about: item.about,
+      file: item.file.url,
+      poster: item.poster?.url,
+      date: item.createdAt,
+      owner: { name: item.owner.name, id: item.owner._id },
+    };
+  });
+
+  res.json({ audios });
+};
+
+export const getPublicProfile: RequestHandler = async (req, res) => {
+  const { profileId } = req.params;
+
+  if (!isValidObjectId(profileId)) {
+    return res.json(422).json({ error: "Invalid profile id!" });
+  }
+
+  const user = await User.findById(profileId);
+
+  if (!user) {
+    return res.json(422).json({ error: "User not found!" });
+  }
+
+  res.json({
+    profile: {
+      id: user._id,
+      name: user.name,
+      followers: user.followers.length,
+      avatar: user.avatar?.url,
+    },
+  });
 };
--- server/src/routers/profile.ts
+++ server/src/routers/profile.ts
@@ -1,9 +1,17 @@
-import { updateFollower } from "#/controllers/profile";
+import {
+  updateFollower,
+  getUploads,
+  getPublicUploads,
+  getPublicProfile,
+} from "#/controllers/profile";
 import { mustAuth } from "#/middleware/auth";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/update-follower/:profileId", mustAuth, updateFollower);
+router.get("/uploads", mustAuth, getUploads);
+router.get("/uploads/:profileId", getPublicUploads);
+router.get("/info/:profileId", getPublicProfile);
 
 export default router;
