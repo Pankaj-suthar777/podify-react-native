b1d1711f95a2b1a67dd95b1ea60f1afb2924995f
pankaj suthar <pankajss0070@gmail.com>
2024-07-25 17:02:33 +0530

    remove single multiple  history

--- PodifyPlayer/src/components/profile/HistoryTab.tsx
+++ PodifyPlayer/src/components/profile/HistoryTab.tsx
@@ -1,42 +1,135 @@
 import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useFetchHistories} from 'src/hooks/query';
 import EmptyRecords from '@ui/EmptyRecords';
 import AudioListLoadingUI from '@ui/AudioListLoadingUI';
 import colors from '@utils/colors';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {getClient} from 'src/api/client';
+import {useMutation, useQueryClient} from 'react-query';
+import {useNavigation} from '@react-navigation/native';
+import {History, historyAudio} from 'src/types/audio';
 
 const HistoryTab = () => {
-  const {data, isLoading} = useFetchHistories();
+  const navigation = useNavigation();
+  const {data, isLoading, isFetching} = useFetchHistories();
+  const queryClient = useQueryClient();
+  const [selectedHistories, setSelectedHistories] = useState<string[]>([]);
+  const noData = !data?.length;
+
+  const removeMutation = useMutation({
+    mutationFn: async histories => removeHistories(histories),
+    onMutate: (histories: string[]) => {
+      queryClient.setQueryData<History[]>(['histories'], oldData => {
+        let newData: History[] = [];
+        if (!oldData) return newData;
+
+        for (let data of oldData) {
+          const filterd = data.audios.filter(
+            item => !histories.includes(item.id),
+          );
+          if (filterd.length) {
+            newData.push({date: data.date, audios: filterd});
+          }
+        }
+
+        return newData;
+      });
+    },
+  });
+
+  const removeHistories = async (histories: string[]) => {
+    const client = await getClient();
+    await client.delete('/history?histories=' + JSON.stringify(histories));
+    queryClient.invalidateQueries({queryKey: ['histories']});
+  };
+
+  const handleSingleHistoryRemove = async (history: historyAudio) => {
+    removeMutation.mutate([history.id]);
+  };
+
+  const handleOnLongPress = async (history: historyAudio) => {
+    setSelectedHistories([history.id]);
+  };
+
+  const handleOnPress = async (history: historyAudio) => {
+    setSelectedHistories(old => {
+      if (old.includes(history.id)) {
+        return old.filter(item => item !== history.id);
+      }
+      return [...old, history.id];
+    });
+  };
+
+  const handleMultipleHistoryRemove = async () => {
+    setSelectedHistories([]);
+    removeMutation.mutate([...selectedHistories]);
+  };
 
   if (isLoading) return <AudioListLoadingUI />;
+
+  useEffect(() => {
+    const unselectHistories = () => {
+      setSelectedHistories([]);
+    };
+    navigation.addListener('blur', unselectHistories);
+    return () => {
+      navigation.removeListener('blur', unselectHistories);
+    };
+  }, []);
 
   if (!data || !data[0]?.audios.length)
     return <EmptyRecords title="There is no history!" />;
 
   return (
-    <ScrollView>
-      {data.map((item, index) => {
-        return (
-          <View key={item.date + index}>
-            <Text style={styles.date}>{item.date}</Text>
-            <View style={styles.listContainer}>
-              {item.audios.map((audio, i) => {
-                return (
-                  <View key={audio.id + i} style={styles.history}>
-                    <Text style={styles.historyTitle}>{audio.title}</Text>
-                    <Pressable>
-                      <AntDesign name="close" color={colors.CONTRAST} />
+    <>
+      {selectedHistories.length ? (
+        <Pressable
+          onPress={handleMultipleHistoryRemove}
+          style={styles.removeBtn}>
+          <Text style={styles.removeBtnText}>Remove</Text>
+        </Pressable>
+      ) : null}
+      <ScrollView>
+        {data.map((item, index) => {
+          return (
+            <View key={item.date + index}>
+              <Text style={styles.date}>{item.date}</Text>
+              <View style={styles.listContainer}>
+                {item.audios.map((audio, i) => {
+                  return (
+                    <Pressable
+                      onLongPress={() => handleOnLongPress(audio)}
+                      onPress={() => handleOnPress(audio)}
+                      key={audio.id + i}
+                      style={styles.history}>
+                      <Text
+                        style={[
+                          styles.historyTitle,
+                          {
+                            backgroundColor: selectedHistories.includes(
+                              audio.id,
+                            )
+                              ? colors.INACTIVE_CONTRAST
+                              : colors.OVERLAY,
+                          },
+                        ]}>
+                        {audio.title}
+                      </Text>
+                      <Pressable
+                        onPress={() => handleSingleHistoryRemove(audio)}>
+                        <AntDesign name="close" color={colors.CONTRAST} />
+                      </Pressable>
                     </Pressable>
-                  </View>
-                );
-              })}
+                  );
+                })}
+              </View>
             </View>
-          </View>
-        );
-      })}
-      <Text>HistoryTab</Text>
-    </ScrollView>
+          );
+        })}
+        <Text>HistoryTab</Text>
+      </ScrollView>
+    </>
   );
 };
 
@@ -63,5 +156,12 @@ const styles = StyleSheet.create({
   listContainer: {
     marginTop: 10,
     paddingLeft: 10,
+  },
+  removeBtn: {
+    padding: 10,
+    alignSelf: 'flex-end',
+  },
+  removeBtnText: {
+    color: colors.CONTRAST,
   },
 });
